0ed7d92f8063760e1922ce90f5d48ac82d8a28f8
José Duarte <duarte.gmj@gmail.com>
2024-03-07 10:29:53 +0000

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,43 @@
+mod simulator;
+
 use std::{
     io::{Read, Write},
     net::TcpStream,
 };
 
-use log::{debug, error, info};
+use log::{debug, error, info, trace};
 use quickcheck::Arbitrary;
+use simulator::Command;
+
+use crate::simulator::Simulator;
 
 #[derive(Debug)]
-enum ExecutionError {
-    InvalidRead,
-    InvalidWrite,
-    Overflow,
-}
-
-struct State([u8; 4]);
-
-impl State {
-    fn new() -> Self {
-        Self([0; 4])
-    }
-
-    fn execute_command(&mut self, command: &Command) -> Result<u8, ExecutionError> {
-        match command {
-            Command::Read(location) => self
-                .0
-                // SAFETY: usize is bigger than u8
-                .get(*location as usize)
-                .copied()
-                .ok_or(ExecutionError::InvalidRead),
-            Command::Write(location, value) => {
-                // SAFETY: usize is bigger than u8
-                if let Some(stored_value) = self.0.get_mut(*location as usize) {
-                    *stored_value = *value;
-                    Ok(*value)
-                } else {
-                    Err(ExecutionError::InvalidWrite)
-                }
-            }
-            Command::Sum => {
-                // .sum doesn't check for overflow
-                let mut acc: u8 = 0;
-                for v in self.0.iter() {
-                    if let Some(res) = acc.checked_add(*v) {
-                        acc = res
-                    } else {
-                        return Err(ExecutionError::Overflow);
-                    }
-                }
-                Ok(acc)
-            }
-            Command::Product => {
-                // .product doesn't check for overflow
-                let mut acc: u8 = 1;
-                for v in self.0.iter() {
-                    if let Some(res) = acc.checked_mul(*v) {
-                        acc = res
-                    } else {
-                        return Err(ExecutionError::Overflow);
-                    }
-                }
-                Ok(acc)
-            }
-        }
-    }
-
-    // fn execute_commands<I: IntoIterator<Item = Command>>(
-    //     &mut self,
-    //     commands: I,
-    // ) -> Result<u8, ExecutionError> {
-    //     let mut last_result = 0;
-    //     for command in commands {
-    //         last_result = self.execute_command(&command)?;
-    //     }
-    //     Ok(last_result)
-    // }
-}
-
-#[derive(Clone, Debug)]
-enum Command {
-    Read(u8),
-    Write(u8, u8),
-    Sum,
-    Product,
-}
-
-impl Command {
-    fn to_bytes(&self) -> [u8; 3] {
-        match self {
-            Command::Read(location) => [1, *location, 0],
-            Command::Write(location, value) => [2, *location, *value],
-            Command::Sum => [3, 0, 0],
-            Command::Product => [4, 0, 0],
-        }
-    }
-}
-
-#[derive(Debug)]
-enum ReadError {
+enum ResponseError {
     Error,
     Invalid,
 }
 
 // I can't re-use Result because of the foreign impl restrictions
+// I can't `impl` it or `impl From` it
 #[derive(Debug)]
 enum Response {
+    /// Execution was successful.
     Success(u8),
-    Failure(ReadError),
+    /// Failed to execute the command.
+    Failure(ResponseError),
 }
 
 impl From<[u8; 2]> for Response {
     fn from(value: [u8; 2]) -> Self {
         match value[0] {
             0 => Response::Success(value[1]),
-            1 => Response::Failure(ReadError::Error),
-            _ => Response::Failure(ReadError::Invalid),
+            1 => Response::Failure(ResponseError::Error),
+            _ => Response::Failure(ResponseError::Invalid),
         }
     }
 }
 
+/// Serialize and send a [`Command`] over the passed stream for execution.
 fn execute_command(stream: &mut TcpStream, command: &Command) -> Result<Response, std::io::Error> {
     stream.write_all(&command.to_bytes())?;
     let mut buffer = [0, 0];
@@ -125,51 +45,20 @@ fn execute_command(stream: &mut TcpStream, command: &Command) -> Result<Response
     Ok(Response::from(buffer))
 }
 
-// TODO: sweep_read — read all 4 state bytes to check for values
-// TODO: assert over sweep_read
-
-impl Arbitrary for Command {
-    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-        // Choosing 0 to 3 since 4 will panic the remote
-        // Typing is easier this way, hence the static
-        static CHOICES: [u8; 4] = [0, 1, 2, 3];
-        fn read(g: &mut quickcheck::Gen) -> Command {
-            let ret = Command::Read(*(g.choose(&CHOICES).unwrap()));
-            ret
-        }
-        fn write(g: &mut quickcheck::Gen) -> Command {
-            let ret = Command::Write(*(g.choose(&CHOICES).unwrap()), u8::arbitrary(g));
-            ret
-        }
-        fn product(_: &mut quickcheck::Gen) -> Command {
-            let ret = Command::Product;
-            ret
-        }
-        fn sum(_: &mut quickcheck::Gen) -> Command {
-            let ret = Command::Sum;
-            ret
-        }
-        // This is a weird dialect but allows for lazyness when generating the cases
-        g.choose(&[
-            read as fn(&mut quickcheck::Gen) -> Command,
-            write as fn(&mut quickcheck::Gen) -> Command,
-            product as fn(&mut quickcheck::Gen) -> Command,
-            sum as fn(&mut quickcheck::Gen) -> Command,
-        ])
-        .unwrap()(g)
-    }
-}
-
 fn main() {
     env_logger::init();
 
-    let mut state = State::new();
+    let mut state = Simulator::new();
+    trace!("initialized simulator");
 
     let mut stream =
         TcpStream::connect("127.0.0.1:10203").expect("connection should be successful");
+    trace!("opened connection");
 
     let mut g = quickcheck::Gen::new(256);
     let mut n_commands = 0;
+    let mut trace: Vec<Command> = vec![];
+
     loop {
         let command = Command::arbitrary(&mut g);
         debug!("generated command: {:?}", command);
@@ -177,32 +66,36 @@ fn main() {
         let local_result = state.execute_command(&command);
         let remote_result = execute_command(&mut stream, &command);
 
+        // Could put it before executing everything but would require a clone.
+        // Pushing the command here ends up saving an allocation/free pair.
+        trace.push(command);
+
         match (local_result, remote_result) {
-            // TODO add better reporting
             (Ok(left), Ok(Response::Success(right))) => {
+                // Using match guards here would lead to the "right"
+                // case falling to the match's last arm
                 if left != right {
                     error!("results diverged! expected {}, received {}", left, right);
-                    info!("number of commands processed: {}", n_commands);
-                    return;
+                    break;
                 }
             }
             (left, Err(e)) => {
-                error!("local result: {:?}", left);
-                error!("remote communication error: {}", e);
-                info!("number of commands processed: {}", n_commands);
-                return;
+                // UnexpectedEof usually means the remote panicked
+                error!("communication error: {} (local result: {:?})", e, left);
+                break;
             }
             (left, Ok(right)) => {
                 error!(
                     "results diverged! expected {:?}, received {:?}",
                     left, right
                 );
-                info!("number of commands processed: {}", n_commands);
-
-                return;
+                break;
             }
         }
 
         n_commands += 1;
     }
+
+    info!("number of commands processed: {}", n_commands);
+    debug!("command trace: {:?}", trace);
 }
---
+++ src/simulator.rs
@@ -0,0 +1,117 @@
+use quickcheck::Arbitrary;
+
+#[derive(Debug)]
+pub enum SimulatorError {
+    InvalidRead,
+    InvalidWrite,
+    Overflow,
+}
+
+/// Simulator for the remote to detect discrepancies.
+pub struct Simulator([u8; 4]);
+
+impl Simulator {
+    /// Create a new [`Simulator`].
+    pub fn new() -> Self {
+        Self([0; 4])
+    }
+
+    /// Execute a [`Command`] on the [`Simulator`].
+    pub fn execute_command(&mut self, command: &Command) -> Result<u8, SimulatorError> {
+        match command {
+            Command::Read(location) => self
+                .0
+                // SAFETY: usize is bigger than u8
+                .get(*location as usize)
+                .copied()
+                .ok_or(SimulatorError::InvalidRead),
+            Command::Write(location, value) => {
+                // SAFETY: usize is bigger than u8
+                if let Some(stored_value) = self.0.get_mut(*location as usize) {
+                    *stored_value = *value;
+                    Ok(*value)
+                } else {
+                    Err(SimulatorError::InvalidWrite)
+                }
+            }
+            // `sum` and `product` don't check for overflow, they'll panic in debug and wrap in release
+            // thus, here I do a checked_X to detect issues with overflow
+            Command::Sum => {
+                // .sum doesn't check for overflow
+                let mut acc: u8 = 0;
+                for v in self.0.iter() {
+                    if let Some(res) = acc.checked_add(*v) {
+                        acc = res
+                    } else {
+                        return Err(SimulatorError::Overflow);
+                    }
+                }
+                Ok(acc)
+            }
+            Command::Product => {
+                let mut acc: u8 = 1;
+                for v in self.0.iter() {
+                    if let Some(res) = acc.checked_mul(*v) {
+                        acc = res
+                    } else {
+                        return Err(SimulatorError::Overflow);
+                    }
+                }
+                Ok(acc)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Command {
+    /// Read a byte at the given index.
+    Read(u8),
+    /// Write a byte (2nd value) at the given index (1st value).
+    Write(u8, u8),
+    /// Sum all values in memory.
+    Sum,
+    /// Multiply all values in memory.
+    Product,
+}
+
+impl Command {
+    pub fn to_bytes(&self) -> [u8; 3] {
+        match self {
+            Command::Read(location) => [1, *location, 0],
+            Command::Write(location, value) => [2, *location, *value],
+            Command::Sum => [3, 0, 0],
+            Command::Product => [4, 0, 0],
+        }
+    }
+}
+
+impl Arbitrary for Command {
+    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+        // Choosing 0 to 3 since 4 will panic the remote, we could add that but leads to not very interesting tests
+        // Typing is easier this way, hence the static
+        static CHOICES: [u8; 5] = [0, 1, 2, 3, 4];
+        fn read(g: &mut quickcheck::Gen) -> Command {
+            Command::Read(*(g.choose(&CHOICES).unwrap()))
+        }
+        fn write(g: &mut quickcheck::Gen) -> Command {
+            Command::Write(*(g.choose(&CHOICES).unwrap()), u8::arbitrary(g))
+        }
+        fn product(_: &mut quickcheck::Gen) -> Command {
+            Command::Product
+        }
+        fn sum(_: &mut quickcheck::Gen) -> Command {
+            Command::Sum
+        }
+        // This is a weird dialect at first sight but allows for lazyness when generating the cases
+        // furthermore, it's simpler when picking a branch because they're built on demand
+        g.choose(&[
+            read as fn(&mut quickcheck::Gen) -> Command,
+            write as fn(&mut quickcheck::Gen) -> Command,
+            product as fn(&mut quickcheck::Gen) -> Command,
+            sum as fn(&mut quickcheck::Gen) -> Command,
+        ])
+        // SAFETY: `choose` docs state that this will never be none if a non-empty slice is passed
+        .unwrap()(g)
+    }
+}
